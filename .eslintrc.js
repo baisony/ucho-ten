ffcf9ecc4bda0188644631d054d63751da770215
So Asano <so.asano@gmail.com>
2023-10-07 15:28:21 +0900

    lint 設定を変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,28 @@
 module.exports = {
-    extends: ['plugin:prettier/recommended'],
-    plugins: ['prettier'],
+    extends: [
+        "plugin:prettier/recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    plugins: ["prettier"],
     rules: {
-        'prettier/prettier': 'error',
+        "prettier/prettier": "error",
+        "no-unused-vars": "warn",
+        "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/ban-ts-comment": [
+            "warn",
+            {
+                "ts-ignore": "allow-with-description",
+                minimumDescriptionLength: 15,
+            },
+        ],
+    },
+    parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+    },
+    env: {
+        es2021: true,
+        node: true,
     },
 }
