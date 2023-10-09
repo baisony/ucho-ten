059371f081edb9675c713aed2026b45764ade9b3
So Asano <so.asano@gmail.com>
2023-10-09 14:57:06 +0900

    ホーム画面のヘッダの上部タブの挙動を改善 (#47)

--- src/app/components/ViewHeader/ViewHeader.tsx
+++ src/app/components/ViewHeader/ViewHeader.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo, useCallback, Key } from "react"
+import React, { useState, useEffect, Key, useRef } from "react"
 import Image from "next/image"
 import { viewHeader } from "./styles"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
@@ -47,17 +47,17 @@ export const ViewHeader: React.FC<Props> = (props: Props) => {
         selectedTab,
     } = props
     const router = useRouter()
-    const reg =
-        /^[\u0009-\u000d\u001c-\u0020\u11a3-\u11a7\u1680\u180e\u2000-\u200f\u202f\u205f\u2060\u3000\u3164\ufeff\u034f\u2028\u2029\u202a-\u202e\u2061-\u2063\ufeff]*$/
+    // const reg =
+    //     /^[\u0009-\u000d\u001c-\u0020\u11a3-\u11a7\u1680\u180e\u2000-\u200f\u202f\u205f\u2060\u3000\u3164\ufeff\u034f\u2028\u2029\u202a-\u202e\u2061-\u2063\ufeff]*$/
     const searchParams = useSearchParams()
     const [searchText, setSearchText] = useState("")
     const target = searchParams.get("target")
-    const [loading, setLoading] = useState(false)
+    const selectedFeed = searchParams.get("feed") || "following"
+    // const [loading, setLoading] = useState(false)
     const [isSideBarOpen, setIsSideBarOpen] = useState<boolean>(false)
     const [isComposing, setComposing] = useState(false)
-    const [pinnedFeeds, setPinnedFeeds] = useState<any>([])
-    const [isFirstTimeHomeTabsSelection, setIsFirstTimeHomeTabsSelection] =
-        useState<boolean>(true)
+    const [pinnedFeeds, setPinnedFeeds] = useState<any[] | null>(null)
+
     const {
         Header,
         HeaderContentTitleContainer,
@@ -66,14 +66,12 @@ export const ViewHeader: React.FC<Props> = (props: Props) => {
         bottom,
         HeaderInputArea,
     } = viewHeader()
-    const AppearanceColor = color
+    // const AppearanceColor = color
 
     useEffect(() => {
-        if (!feedGenerators) {
-            return
+        if (feedGenerators) {
+            setPinnedFeeds(feedGenerators)
         }
-
-        setPinnedFeeds(feedGenerators)
     }, [feedGenerators])
 
     useEffect(() => {
@@ -83,15 +81,7 @@ export const ViewHeader: React.FC<Props> = (props: Props) => {
     }, [])
 
     const handleHomeTopTabSelectionChange = (e: Key) => {
-        if (isFirstTimeHomeTabsSelection) {
-            setIsFirstTimeHomeTabsSelection(false)
-
-            if (pathname === "" && !searchParams.get("feed")) {
-                router.push(`/?feed=following`)
-            }
-        } else {
-            router.push(`/?feed=${e}`)
-        }
+        router.push(`/?feed=${e}`)
     }
 
     return (
@@ -187,12 +177,13 @@ export const ViewHeader: React.FC<Props> = (props: Props) => {
                 orientation="horizontal"
                 hideScrollBar
             >
-                {selectedTab === "home" && (
+                {selectedTab === "home" && pinnedFeeds && (
                     <Tabs
                         aria-label="Options"
                         color="primary"
                         variant="underlined"
-                        items={pinnedFeeds}
+                        // items={["following", ...pinnedFeeds]}
+                        selectedKey={selectedFeed}
                         onSelectionChange={handleHomeTopTabSelectionChange}
                         classNames={{
                             tabList:
@@ -203,16 +194,14 @@ export const ViewHeader: React.FC<Props> = (props: Props) => {
                         }}
                         style={{ marginLeft: "40px" }}
                     >
-                        {userPreferences && (
-                            <Tab
-                                key="following"
-                                title={
-                                    <div className="flex items-center pl-[15px] pr-[15px]">
-                                        <span>Following</span>
-                                    </div>
-                                }
-                            />
-                        )}
+                        <Tab
+                            key="following"
+                            title={
+                                <div className="flex items-center pl-[15px] pr-[15px]">
+                                    <span>Following</span>
+                                </div>
+                            }
+                        />
                         {pinnedFeeds.map((feed: any, index: number) => (
                             <Tab
                                 key={feed.uri}
@@ -221,9 +210,6 @@ export const ViewHeader: React.FC<Props> = (props: Props) => {
                                         <span>{feed.displayName}</span>
                                     </div>
                                 }
-                                onClick={() => {
-                                    console.log(feed.uri)
-                                }}
                             />
                         ))}
                     </Tabs>
