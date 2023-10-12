d3d18cdb6d09e5ff7e56dcc14e893f7e6e60bda3
ば <baisoni.baisony@gmail.com>
2023-10-12 23:54:49 +0900

    コードの無駄な部分を削除

--- src/app/components/ViewQuoteCard/ViewQuoteCard.tsx
+++ src/app/components/ViewQuoteCard/ViewQuoteCard.tsx
@@ -1,42 +1,10 @@
 import React, { useCallback, useMemo, useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {
-    faComment,
-    faStar as faHeartRegular,
-} from "@fortawesome/free-regular-svg-icons"
-import {
-    faCheckCircle,
-    faCircleQuestion,
-    faCircleXmark,
-    faCode,
-    faEllipsis,
-    faFlag,
-    faHashtag,
-    faLink,
-    faReply,
-    faRetweet,
-    faStar as faHeartSolid,
-    faTrash,
-    faUser,
-} from "@fortawesome/free-solid-svg-icons"
+import { faUser } from "@fortawesome/free-solid-svg-icons"
 import { PostModal } from "../PostModal"
 import { Linkcard } from "../Linkcard"
 import "react-circular-progressbar/dist/styles.css"
-import {
-    Chip,
-    Dropdown,
-    DropdownItem,
-    DropdownMenu,
-    DropdownSection,
-    DropdownTrigger,
-    Image,
-    Modal,
-    ModalContent,
-    ScrollShadow,
-    Skeleton,
-    Tooltip,
-    useDisclosure,
-} from "@nextui-org/react"
+import { Image, ScrollShadow, Skeleton } from "@nextui-org/react"
 import "react-swipeable-list/dist/styles.css"
 import { useAgent } from "@/app/_atoms/agent"
 import { useRouter } from "next/navigation"
@@ -87,32 +55,18 @@ export const ViewQuoteCard: React.FC<Props> = (props: Props) => {
         PostCard,
         PostAuthor,
         PostContent,
-        PostReactionButtonContainer,
         PostCardContainer,
-        PostReactionButton,
         PostAuthorIcon,
         PostAuthorDisplayName,
         PostAuthorHandle,
         PostCreatedAt,
-        dropdown,
         skeletonIcon,
         skeletonName,
         skeletonHandle,
         skeletonText1line,
         skeletonText2line,
-        chip,
-        LinkCard,
-        LinkCardThumbnailContainer,
-        LinkCardThumbnail,
-        LinkCardContent,
-        LinkCardTitle,
-        LinkCardDescription,
-        LinkCardSiteName,
     } = viewQuoteCard()
     const [isSwipeEnabled, setIsSwipeEnabled] = useState(true)
-    const [postInfo, setPostInfo] = useState<any>(null)
-    const [isTextSelectionInProgress, setIsTextSelectionInProgress] =
-        useState(false)
     const [startX, setStartX] = useState(null)
     const [startY, setStartY] = useState(null)
 
@@ -159,8 +113,6 @@ export const ViewQuoteCard: React.FC<Props> = (props: Props) => {
     const renderTextWithLinks = useMemo(() => {
         console.log(postJson)
         if (!postJson?.value) return
-        const encoder = new TextEncoder()
-        const decoder = new TextDecoder()
         if (true) {
             const post: any[] = []
             postJson.value.text.split("\n").map((line: any, i: number) => {
@@ -173,168 +125,6 @@ export const ViewQuoteCard: React.FC<Props> = (props: Props) => {
             })
             return post
         }
-        const { text, facets } = postJson.value
-        const text_bytes = encoder.encode(text)
-        const result: any[] = []
-        let lastOffset = 0
-        facets.forEach((facet: any, index: number) => {
-            const { byteStart, byteEnd } = facet.index
-
-            const facetText = decoder.decode(
-                text_bytes.slice(byteStart, byteEnd)
-            )
-
-            // 直前のテキストを追加
-            if (byteStart > lastOffset) {
-                const nonLinkText = decoder.decode(
-                    text_bytes.slice(lastOffset, byteStart)
-                )
-                const textChunks = nonLinkText
-                    .split("\n")
-                    .map((line, index, array) => (
-                        <span key={`text-${byteStart}-${index}`}>
-                            {line}
-                            {index !== array.length - 1 && <br />}
-                        </span>
-                    ))
-                result.push(textChunks)
-            }
-
-            switch (facet.features[0].$type) {
-                case "app.bsky.richtext.facet#mention":
-                    result.push(
-                        <span
-                            key={`link-${index}-${byteStart}`}
-                            className={"text-blue-500"}
-                            onClick={(e) => {
-                                e.preventDefault()
-                                e.stopPropagation()
-                                router.push(`/profile/${facet.features[0].did}`)
-                            }}
-                        >
-                            {facetText}
-                        </span>
-                    )
-                    break
-
-                case "app.bsky.richtext.facet#link":
-                    result.push(
-                        <span key={`link-${index}-${byteStart}`}>
-                            <Chip
-                                className={chip({ color: color })}
-                                startContent={
-                                    <Tooltip
-                                        showArrow={true}
-                                        color={"foreground"}
-                                        content={
-                                            facetText === facet.features[0].uri
-                                                ? "リンク偽装の心配はありません。"
-                                                : facet.features[0].uri.includes(
-                                                      facetText.replace(
-                                                          "...",
-                                                          ""
-                                                      )
-                                                  )
-                                                ? "URL短縮の可能性があります。"
-                                                : "リンク偽装の可能性があります。"
-                                        }
-                                    >
-                                        <FontAwesomeIcon
-                                            icon={
-                                                facetText ===
-                                                facet.features[0].uri
-                                                    ? faCheckCircle
-                                                    : facet.features[0].uri.includes(
-                                                          facetText.replace(
-                                                              "...",
-                                                              ""
-                                                          )
-                                                      )
-                                                    ? faCircleQuestion
-                                                    : faCircleXmark
-                                            }
-                                        />
-                                    </Tooltip>
-                                }
-                                variant="faded"
-                                color={
-                                    facetText === facet.features[0].uri
-                                        ? "success"
-                                        : facet.features[0].uri.includes(
-                                              facetText.replace("...", "")
-                                          )
-                                        ? "default"
-                                        : "danger"
-                                }
-                            >
-                                {facet.features[0].uri.startsWith(
-                                    "https://bsky.app"
-                                ) ? (
-                                    <span
-                                        key={`a-${index}-${byteStart}`}
-                                        onClick={(e) => {
-                                            e.preventDefault()
-                                            e.stopPropagation()
-                                            router.push(
-                                                facet.features[0].uri.replace(
-                                                    "https://bsky.app",
-                                                    `${location.protocol}//${window.location.host}`
-                                                )
-                                            )
-                                        }}
-                                    >
-                                        {facetText}
-                                    </span>
-                                ) : (
-                                    <a
-                                        onMouseUp={(e) => e.stopPropagation()}
-                                        key={`a-${index}-${byteStart}`}
-                                        href={facet.features[0].uri}
-                                        target={"_blank"}
-                                        rel={"noopener noreferrer"}
-                                    >
-                                        {facetText}
-                                    </a>
-                                )}
-                            </Chip>
-                        </span>
-                    )
-                    break
-
-                case "app.bsky.richtext.facet#tag":
-                    result.push(
-                        <span key={`link-${index}-${byteStart}`}>
-                            <Chip
-                                className={chip({ color: color })}
-                                startContent={
-                                    <FontAwesomeIcon icon={faHashtag} />
-                                }
-                                variant="faded"
-                                color="primary"
-                            >
-                                <span
-                                    key={`a-${index}-${byteStart}`}
-                                    onClick={(e) => {
-                                        e.preventDefault()
-                                        e.stopPropagation()
-                                        router.push(
-                                            `/search?word=%23${facet.features[0].tag.replace(
-                                                "#",
-                                                ""
-                                            )}&target=posts`
-                                        )
-                                    }}
-                                >
-                                    {facetText.replace("#", "")}
-                                </span>
-                            </Chip>
-                        </span>
-                    )
-                    break
-            }
-            lastOffset = byteEnd
-        })
-        return result
     }, [])
 
     const handleMouseUp = (e: any) => {
@@ -421,7 +211,7 @@ export const ViewQuoteCard: React.FC<Props> = (props: Props) => {
                                                         isEmbedToModal
                                                             ? `z-[2]`
                                                             : `z-[0]`
-                                                    } rounded-[10px]`}
+                                                    } rounded-[7px]`}
                                                     alt={postJson?.author.did}
                                                 />
                                             ) : (
