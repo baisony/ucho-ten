8867c9fba9327fab4ca4fe605379511ce6d3f603
ば <baisoni.baisony@gmail.com>
2023-10-18 23:07:54 +0900

    各ページのタブに出現する項目を定義 #179

--- src/app/_atoms/headerMenus.ts
+++ src/app/_atoms/headerMenus.ts
@@ -1,5 +1,4 @@
 import { atom, useAtom } from "jotai"
-
 import { HeaderMenu } from "@/app/_atoms/headerMenu"
 
 const defaultHomeMenus: HeaderMenu[] = []
@@ -42,16 +41,68 @@ const defaultProfileMenus: HeaderMenu[] = [
     },
 ]
 
+const defaultOnlyPostMenus: HeaderMenu[] = [
+    {
+        displayText: "Author's",
+        info: "Author's",
+    },
+    {
+        displayText: "Other's",
+        info: "Other's",
+    },
+]
+
+const defaultSettingsMenus: HeaderMenu[] = [
+    {
+        displayText: "Settings",
+        info: "Settings",
+    },
+    {
+        displayText: "General",
+        info: "General",
+    },
+    {
+        displayText: "Content Filtering",
+        info: "Content Filtering",
+    },
+]
+const defaultBookmarksMenus: HeaderMenu[] = [
+    {
+        displayText: "Bookmark",
+        info: "Bookmark",
+    },
+]
+const defaultMytFeedMenus: HeaderMenu[] = [
+    {
+        displayText: "My Feeds",
+        info: "My Feeds",
+    },
+    {
+        displayText: "Search",
+        info: "Search",
+    },
+]
+
 interface AllOfHeaderMenus {
     home: HeaderMenu[]
     search: HeaderMenu[]
     inbox: HeaderMenu[]
+    profile: HeaderMenu[]
+    onlyPost: HeaderMenu[]
+    settings: HeaderMenu[]
+    bookmarks: HeaderMenu[]
+    myFeed: HeaderMenu[]
 }
 
 const initialHeaderMenus: AllOfHeaderMenus = {
     home: defaultHomeMenus,
     search: defaultSearchMenus,
-    inbox: defaultSearchMenus,
+    inbox: defaultInboxMenus,
+    profile: defaultProfileMenus,
+    onlyPost: defaultOnlyPostMenus,
+    settings: defaultSettingsMenus,
+    bookmarks: defaultBookmarksMenus,
+    myFeed: defaultMytFeedMenus,
 }
 
 const allOfHeaderMenus = atom<AllOfHeaderMenus>(initialHeaderMenus)
