f844a66f2aeed1b355215c5b993d42aabe6b75bd
ば <baisoni.baisony@gmail.com>
2023-10-22 22:23:30 +0900

    fix error

--- src/app/components/PostModal/PostModal.tsx
+++ src/app/components/PostModal/PostModal.tsx
@@ -83,8 +83,6 @@ export const PostModal: React.FC<Props> = (props: Props) => {
 
     const [agent, setAgent] = useAgent()
     const router = useRouter()
-    const searchParams = useSearchParams()
-    const postParam = searchParams.get("text")
     const reg =
         /^[\u0009-\u000d\u001c-\u0020\u11a3-\u11a7\u1680\u180e\u2000-\u200f\u202f\u205f\u2060\u3000\u3164\ufeff\u034f\u2028\u2029\u202a-\u202e\u2061-\u2063]*$/
     const [PostContentLanguage, setPostContentLanguage] = useState(
