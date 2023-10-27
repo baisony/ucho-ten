bbc2d8dab9cf3c16977a2716991e4dd5ca840083
ば <baisoni.baisony@gmail.com>
2023-10-27 16:07:44 +0900

    Update noflash.js

--- public/noflash.js
+++ public/noflash.js
@@ -6,7 +6,7 @@
         if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
             document.documentElement.classList.add("dark")
         }
-        localStorage.setItem("appearanceColor", "system")
+        localStorage.setItem("appearanceColor", '"system"')
     } else if (JSON.parse(localStorage.getItem("appearanceColor")) === "dark") {
         document.documentElement.classList.add("dark")
     } else {
