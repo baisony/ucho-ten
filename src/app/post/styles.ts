5a08d94afdf78872e8ed23cc09aafc273ca738f0
ば <baisoni.baisony@gmail.com>
2023-10-27 16:36:49 +0900

    bg-opacityを設定

--- src/app/post/styles.ts
+++ src/app/post/styles.ts
@@ -6,7 +6,7 @@ export const createPostPage = tv({
         backgroundColor: "w-full h-full absolute",
 
         PostModal:
-            "text-white dark:text-[#D7D7D7] bg-[#DADADA] dark:bg-[#2C2C2C] w-full min-w-[300px] max-w-[500px] h-full max-h-[350px] shadow-xl relative bg-opacity-70 rounded-none md:rounded-[10px] md:overflow-hidden md:min-h-[400px]",
+            "text-white dark:text-[#D7D7D7] bg-[#DADADA] dark:bg-[#2C2C2C] bg-opacity-70 backdrop-blur-[15px] dark:bg-opacity-50 dark:backdrop-blur-[15px] w-full min-w-[300px] max-w-[500px] h-full max-h-[350px] shadow-xl relative rounded-none md:rounded-[10px] md:overflow-hidden md:min-h-[400px]",
         header: "w-full h-[43px] select-none flex justify-between items-center",
         headerCancelButton: "w-[91px] h-[37px] left-[4px] text-white",
         headerTitle:
