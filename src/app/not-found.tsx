a99b1a9676da2b3f4db96cdd06e00c0e2b4b1ec8
ば <baisoni.baisony@gmail.com>
2023-11-26 06:52:08 +0900

    404ページに背景色が設定されて、４０４表示が見えない問題を修正 #465

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,19 @@
 import zazen from "@/../public/images/404page/zazen_obousan.png"
 import fortyfour from "@/../public/images/404page/404.png"
 import Image from "next/image"
+import { useStatusCodeAtPage } from "@/app/_atoms/statusCode"
+import { useEffect } from "react"
+import { useCurrentMenuType } from "@/app/_atoms/headerMenu"
 
 const PageNotFound = () => {
+    const [, setStatusCode] = useStatusCodeAtPage()
+    const [currentMenuType, setCurrentMenuType] = useCurrentMenuType()
+    setCurrentMenuType("notFound")
+    useEffect(() => {
+        setStatusCode(404)
+    }, [])
     return (
-        <div className="w-full h-full flex justify-center ">
+        <div className="w-full h-full flex justify-center items-center">
             <div className="flex flex-col items-center ">
                 <Image src={fortyfour} alt="404" />
                 <Image src={zazen} alt="404" height={150} className="mt-5" />
