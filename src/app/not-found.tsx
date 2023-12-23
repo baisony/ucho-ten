d01f024a1621a47adcec8c1c4c15e207d3e013fe
ば <baisoni.baisony@gmail.com>
2023-12-23 16:49:45 +0900

    update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,8 +5,10 @@ import Image from "next/image"
 import { useStatusCodeAtPage } from "@/app/_atoms/statusCode"
 import { useEffect } from "react"
 import { useCurrentMenuType } from "@/app/_atoms/headerMenu"
+import { useRouter } from "next/navigation"
 
 const PageNotFound = () => {
+    const router = useRouter()
     const [, setStatusCode] = useStatusCodeAtPage()
     const [, setCurrentMenuType] = useCurrentMenuType()
     setCurrentMenuType("notFound")
@@ -15,9 +17,15 @@ const PageNotFound = () => {
     }, [])
     return (
         <div className="w-full h-full flex justify-center items-center">
-            <div className="flex flex-col items-center ">
+            <div
+                className="flex flex-col items-center hover:cursor-pointer text-black dark:text-white"
+                onClick={() => {
+                    router.back()
+                }}
+            >
                 <Image src={fortyfour} alt="404" />
                 <Image src={zazen} alt="404" height={150} className="mt-5" />
+                <div>Go back</div>
             </div>
         </div>
     )
