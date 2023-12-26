b3ddc4e0c583b5e6aad2aa2e2c43e4bb189a3a16
ば <baisoni.baisony@gmail.com>
2023-12-26 20:18:39 +0900

    commit

--- src/app/_atoms/contentFontSize.ts
+++ src/app/_atoms/contentFontSize.ts
@@ -1,6 +1,9 @@
 import { useAtom } from "jotai"
 import { atomWithStorage } from "jotai/utils"
 
-const fontsize = atomWithStorage<1 | 2 | 3 | 4 | 5>("fontsize", 3)
+const fontsize = atomWithStorage<1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10>(
+    "fontsize",
+    3
+)
 
 export const useContentFontSize = () => useAtom(fontsize)
