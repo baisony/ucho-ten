8a34d8c6f25e1e00cb9d1e15c04feff101f52f67
ば <baisoni.baisony@gmail.com>
2023-12-28 18:19:18 +0900

    Update DummyHeader.tsx

--- src/app/_components/DummyHeader/DummyHeader.tsx
+++ src/app/_components/DummyHeader/DummyHeader.tsx
@@ -8,7 +8,6 @@ export const DummyHeader: React.FC<Props> = (props: Props) => {
     if (props.isSearchScreen) {
         return <div className={"lg:h-[100px] md:h-[100px] h-[85px]"} />
     } else {
-        z
         return <div className={"lg:h-[50px] md:h-[100px] h-[85px]"} />
     }
 }
