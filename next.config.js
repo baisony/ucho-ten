8faf2a0d14459536a5951ff3d2c31973b0b9ca41
ば <baisoni.baisony@gmail.com>
2023-12-31 03:19:07 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+const plugin = require("tailwindcss/plugin")
 
 const withPWA = require("next-pwa")({
     dest: "public",
@@ -8,4 +9,33 @@ const withPWA = require("next-pwa")({
 module.exports = withPWA({
     //next.js config
     reactStrictMode: false,
+    plugins: [
+        {
+            tailwindcss: {},
+            autoprefixer: {},
+            ...(process.env.NODE_ENV === "development" ? { cssnano: {} } : {}),
+        },
+        plugin(function ({ addUtilities }) {
+            addUtilities({
+                ".contain-strict": {
+                    contain: "strict",
+                },
+                ".contain-content": {
+                    contain: "content",
+                },
+                ".contain-size": {
+                    contain: "size",
+                },
+                ".contain-layout": {
+                    contain: "layout",
+                },
+                ".contain-style": {
+                    contain: "style",
+                },
+                ".contain-paint": {
+                    contain: "paint",
+                },
+            })
+        }),
+    ],
 })
