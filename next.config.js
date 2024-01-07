5d929d1aac96473a3b94d5304adf5102cfd7447d
ば <baisoni.baisony@gmail.com>
2024-01-07 13:34:26 +0900

    commit

--- next.config.js
+++ next.config.js
@@ -1,30 +1,8 @@
 /** @type {import('next').NextConfig} */
 const withPlugins = require("next-compose-plugins")
 const TerserPlugin = require("terser-webpack-plugin")
-
-const withTeresrPlugin = (nextConfig = {}) => {
-    return {
-        ...nextConfig,
-        webpack(config, options) {
-            if (!options.isServer) {
-                config.optimization.minimizer = [
-                    new TerserPlugin({
-                        terserOptions: {
-                            compress: {
-                                drop_console: true,
-                            },
-                        },
-                        extractComments: "all",
-                    }),
-                ]
-            }
-            if (typeof nextConfig.webpack === "function") {
-                return nextConfig.webpack(config, options)
-            }
-            return config
-        },
-    }
-}
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts")
 
 const withPWA = require("next-pwa")({
     dest: "public",
@@ -32,7 +10,59 @@ const withPWA = require("next-pwa")({
     skipWaiting: true,
 })
 
-module.exports = withPlugins([withPWA, withTeresrPlugin], {
+const plugins = [
+    withPWA,
+    new TerserPlugin({
+        parallel: 4,
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                ecma: 5,
+                warnings: false,
+                comparisons: false,
+                inline: 2,
+            },
+            mangle: {
+                safari10: true,
+            },
+        },
+        extractComments: "all",
+    }),
+    new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+    }),
+    new RemoveEmptyScriptsPlugin(),
+]
+
+const rules = [
+    {
+        test: /\.module\.scss$/,
+        use: [
+            MiniCssExtractPlugin.Loader,
+            {
+                loader: "css-loader",
+            },
+            {
+                loader: "postcss-loader",
+                options: {
+                    postcssOptions: {
+                        plugins: [["postcss-preset-env"]],
+                    },
+                },
+            },
+            {
+                loader: "thread-loader",
+                options: {
+                    workerParallelJobs: 2,
+                },
+            },
+            "sass-loader",
+        ].filter(Boolean),
+    },
+]
+
+module.exports = withPlugins(plugins, rules, {
     reactStrictMode: false,
     plugins: [
         {
@@ -41,4 +71,12 @@ module.exports = withPlugins([withPWA, withTeresrPlugin], {
             cssnano: {},
         },
     ],
+    optimazation: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin(),
+            new MiniCssExtractPlugin(),
+            new RemoveEmptyScriptsPlugin(),
+        ],
+    },
 })
