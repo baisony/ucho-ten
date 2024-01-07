6bededb083dc3511ef367d25fea6de3472359b28
ば <baisoni.baisony@gmail.com>
2024-01-07 14:10:25 +0900

    Revert "webpack圧縮導入"
    This reverts commit aef283f07b78243c172d95ee69919f1110780b89.

--- next.config.js
+++ next.config.js
@@ -1,38 +1,12 @@
 /** @type {import('next').NextConfig} */
-const withPlugins = require("next-compose-plugins")
-const TerserPlugin = require("terser-webpack-plugin")
-
-const withTeresrPlugin = (nextConfig = {}) => {
-    return {
-        ...nextConfig,
-        webpack(config, options) {
-            if (!options.isServer) {
-                config.optimization.minimizer = [
-                    new TerserPlugin({
-                        terserOptions: {
-                            compress: {
-                                drop_console: true,
-                            },
-                        },
-                        extractComments: "all",
-                    }),
-                ]
-            }
-            if (typeof nextConfig.webpack === "function") {
-                return nextConfig.webpack(config, options)
-            }
-            return config
-        },
-    }
-}
 
 const withPWA = require("next-pwa")({
     dest: "public",
     register: true,
     skipWaiting: true,
 })
-
-module.exports = withPlugins([withPWA, withTeresrPlugin], {
+module.exports = withPWA({
+    //next.js config
     reactStrictMode: false,
     plugins: [
         {
