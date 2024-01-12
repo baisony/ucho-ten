c23c209e7e744107a334bde397cce4f5571cabeb
ば <baisoni.baisony@gmail.com>
2024-01-12 14:50:45 +0900

    didでloginすることでpush通知の受け取りを可能に

--- src/app/_lib/notify/onesignal.tsx
+++ src/app/_lib/notify/onesignal.tsx
@@ -2,17 +2,21 @@
 
 import { useEffect } from "react"
 import OneSignal from "react-onesignal"
+import { useAgent } from "@/app/_atoms/agent"
 
 export const OneSignalInitial = () => {
+    const [agent] = useAgent()
     useEffect(() => {
+        if (!agent) return
         const oneSignalInit = async () => {
             await OneSignal.init({
                 appId: process.env.NEXT_PUBLIC_ONE_SIGNAL_APP_ID || "",
             }).then(() => {
                 OneSignal.Slidedown.promptPush()
             })
+            console.log(await OneSignal.login(agent?.session?.did ?? ""))
         }
         oneSignalInit()
-    }, [])
+    }, [agent])
     return null
 }
