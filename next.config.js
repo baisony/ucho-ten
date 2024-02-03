1a6e101eae59aa4ac5db5851cf7f97fabf92d1c5
ば <baisoni.baisony@gmail.com>
2024-02-03 20:59:52 +0900

    Revert "delete offline"
    This reverts commit aa2e579ff4baeb179021c88a830e1fb0b41edaab.

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const withPWA = require("next-pwa")({
     skipWaiting: true,
     register: false,
     cacheOnFrontEndNav: true,
+    reloadOnOnline: true,
+    fallbacks: {
+        document: "/offline",
+    },
 })
 module.exports = withPWA({
     //next.js config
