5b00f47e7f5bb136cd48c2ec9f5857c53f7d9c23
ば <baisoni.baisony@gmail.com>
2024-02-07 10:19:20 +0900

    recordWithMediaにて表示されないことがある #652

--- src/app/_components/ViewPostCard/EmbedMedia.tsx
+++ src/app/_components/ViewPostCard/EmbedMedia.tsx
@@ -8,6 +8,7 @@ import { GeneratorView } from "@atproto/api/dist/client/types/app/bsky/feed/defs
 import { ViewMuteListCard } from "@/app/_components/ViewMuteListCard"
 import { ListView } from "@atproto/api/dist/client/types/app/bsky/graph/defs"
 import { ViewNotFoundCard } from "@/app/_components/ViewNotFoundCard"
+import { Linkcard } from "@/app/_components/Linkcard"
 
 interface EmbedMediaProps {
     embedMedia: AppBskyEmbedRecordWithMedia.View
@@ -23,45 +24,53 @@ export const EmbedMedia = memo(
         isEmbedToModal,
         nextQueryParams,
     }: EmbedMediaProps) => {
-        const images = embedMedia.media.images
-
-        if (!images || !Array.isArray(images)) {
-            return
-        }
+        const images = embedMedia.media?.images
 
         return (
             !isEmbedToModal && (
                 <>
-                    <ScrollShadow
-                        isEnabled={images.length > 1}
-                        hideScrollBar
-                        orientation="horizontal"
-                        className={`flex overflow-x-auto overflow-y-hidden w-100svw}]`}
-                    >
-                        {images.map((image: ViewImage, index: number) => (
-                            <div
-                                className={`mt-[10px] mb-[10px] rounded-[7.5px] overflow-hidden min-w-[280px] max-w-[500px] h-[300px] mr-[10px] bg-cover`}
-                                key={`image-${index}`}
-                            >
-                                <img
-                                    className={`${
-                                        images.length !== 1
-                                            ? `w-[280px]`
-                                            : "w-full max-w-[500px]"
-                                    } h-[300px] z-0 object-cover`}
-                                    src={image.thumb}
-                                    alt={image.alt}
-                                    onClick={(e) => {
-                                        e.stopPropagation()
-                                        onImageClick(images, index)
-                                    }}
-                                    decoding={"async"}
-                                    loading={"eager"}
-                                    fetchPriority={"high"}
-                                />
-                            </div>
-                        ))}
-                    </ScrollShadow>
+                    {images && (
+                        <ScrollShadow
+                            isEnabled={(images as ViewImage[]).length > 1}
+                            hideScrollBar
+                            orientation="horizontal"
+                            className={`flex overflow-x-auto overflow-y-hidden w-100svw}]`}
+                        >
+                            {(images as ViewImage[]).map(
+                                (image: ViewImage, index: number) => (
+                                    <div
+                                        className={`mt-[10px] mb-[10px] rounded-[7.5px] overflow-hidden min-w-[280px] max-w-[500px] h-[300px] mr-[10px] bg-cover`}
+                                        key={`image-${index}`}
+                                    >
+                                        <img
+                                            className={`${
+                                                (images as ViewImage[])
+                                                    .length !== 1
+                                                    ? `w-[280px]`
+                                                    : "w-full max-w-[500px]"
+                                            } h-[300px] z-0 object-cover`}
+                                            src={image.thumb}
+                                            alt={image.alt}
+                                            onClick={(e) => {
+                                                e.stopPropagation()
+                                                onImageClick(
+                                                    images as ViewImage[],
+                                                    index
+                                                )
+                                            }}
+                                            decoding={"async"}
+                                            loading={"eager"}
+                                            fetchPriority={"high"}
+                                        />
+                                    </div>
+                                )
+                            )}
+                        </ScrollShadow>
+                    )}
+                    {embedMedia.media.$type ===
+                        "app.bsky.embed.external#view" && (
+                        <Linkcard ogpData={embedMedia.media.external} />
+                    )}
                     {embedMedia.record.record.$type ===
                         "app.bsky.embed.record#view" && (
                         <ViewQuoteCard
