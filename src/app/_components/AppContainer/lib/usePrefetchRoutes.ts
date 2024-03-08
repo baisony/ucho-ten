5e6fee361a6f3dc7867157bd355374600bffbed9
ば <baisoni.baisony@gmail.com>
2024-03-08 23:37:44 +0900

    prefetchするページを最適化

--- src/app/_components/AppContainer/lib/usePrefetchRoutes.ts
+++ src/app/_components/AppContainer/lib/usePrefetchRoutes.ts
@@ -2,15 +2,11 @@ import { useEffect } from "react"
 
 export const usePrefetchRoutes = (router: any) => {
     useEffect(() => {
-        router.prefetch("/")
-        router.prefetch("/home")
-        router.prefetch("/login")
         router.prefetch("/search")
         router.prefetch("/u-tab")
         router.prefetch("/inbox")
         router.prefetch("/post")
         router.prefetch("/settings")
-        router.prefetch("/bookmarks")
         router.prefetch("/feeds")
     }, [router])
 }
