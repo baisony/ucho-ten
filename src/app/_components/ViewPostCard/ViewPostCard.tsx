7afe82920cfb035c93e6eade3d831202faae7ccb
ば <baisoni.baisony@gmail.com>
2024-04-14 12:10:32 +0900

    Update ViewPostCard.tsx

--- src/app/_components/ViewPostCard/ViewPostCard.tsx
+++ src/app/_components/ViewPostCard/ViewPostCard.tsx
@@ -445,9 +445,9 @@ export const ViewPostCard = memo((props: ViewPostCardProps) => {
                                     e.stopPropagation()
                                     handleChangeSaveScrollPosition()
                                 }}
-                                href={`/profile/${(
-                                    json?.reason?.by as ProfileViewBasic
-                                )?.did}?${nextQueryParams.toString()}`}
+                                href={`/profile/${
+                                    (json?.reason?.by as ProfileViewBasic)?.did
+                                }?${nextQueryParams.toString()}`}
                             >
                                 <FontAwesomeIcon icon={faRetweet} /> Reposted by{" "}
                                 {(json.reason.by as ProfileViewBasic)
@@ -469,8 +469,9 @@ export const ViewPostCard = memo((props: ViewPostCardProps) => {
                                         e.stopPropagation()
                                         handleChangeSaveScrollPosition()
                                     }}
-                                    href={`/profile/${postJsonData?.author
-                                        ?.did}?${nextQueryParams.toString()}`}
+                                    href={`/profile/${
+                                        postJsonData?.author?.did
+                                    }?${nextQueryParams.toString()}`}
                                 >
                                     <img
                                         src={
@@ -493,8 +494,9 @@ export const ViewPostCard = memo((props: ViewPostCardProps) => {
                                         e.stopPropagation()
                                         handleChangeSaveScrollPosition()
                                     }}
-                                    href={`/profile/${postJsonData?.author
-                                        ?.did}?${nextQueryParams.toString()}`}
+                                    href={`/profile/${
+                                        postJsonData?.author?.did
+                                    }?${nextQueryParams.toString()}`}
                                     className={"items-start"}
                                 >
                                     <span
@@ -515,9 +517,9 @@ export const ViewPostCard = memo((props: ViewPostCardProps) => {
                                                 e.stopPropagation()
                                                 handleChangeSaveScrollPosition()
                                             }}
-                                            href={`/profile/${postJsonData
-                                                ?.author
-                                                ?.did}?${nextQueryParams.toString()}`}
+                                            href={`/profile/${
+                                                postJsonData?.author?.did
+                                            }?${nextQueryParams.toString()}`}
                                         >
                                             <span
                                                 className={`${PostAuthorHandle()} md:hover:underline ml-[5px]`}
